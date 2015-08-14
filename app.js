013ca010f434b137683fb613dac8c891427a5d05
Andy Edwards <andyhedwards@gmail.com>
2015-08-14 20:05:59 +1200

    New site

---
+++ app.js
@@ -0,0 +1,16 @@
+// var main = function(){
+//  $(".nav").hide();
+
+// }
+// $(document).ready(main)
+
+// (function($) {
+//     $(document).ready(function() {
+//          $('html, body').animate({
+//            'scrollTop':   $('#portfolio').offset().top
+//          }, 2000);
+//     });
+// })(jQuery);
+// var main = function(){
+// 	$(".nav")
+// }
